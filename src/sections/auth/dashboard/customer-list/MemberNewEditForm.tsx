57c2341a7fd6443c1ed2622191eb7e622d58f066
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-03-11 20:59:04 +0545

    Member edit form

---
+++ src/sections/auth/dashboard/customer-list/MemberNewEditForm.tsx
@@ -0,0 +1,360 @@
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
+import { Member } from "../../../../pages/dashboard/admin/members/list2";
+import * as Yup from "yup";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { createMember, updateMember } from "../../../../api/member";
+import { PATH_DASHBOARD } from "../../../../routes/path";
+import { FormProvider, RHFTextField } from "../../../../components/hook-form";
+import {
+  Grid,
+  Card,
+  Box,
+  Typography,
+  Stack,
+  Button,
+  FormControlLabel,
+  Switch,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { RHFUploadAvatar } from "../../../../components/hook-form/RHFUpload";
+import Iconify from "../../../../components/Iconify";
+import axios from "axios";
+type Props = {
+  isEdit?: boolean;
+  currentMember?: Member;
+};
+export interface CustomFile extends File {
+  path?: string;
+  preview?: string | ArrayBuffer | null;
+  lastModifiedDate?: Date;
+}
+
+interface FormValuesProps extends Omit<Member, "avatarUrl"> {
+  avatarUrl: CustomFile | string | null;
+}
+
+export default function MemberNewEditForm({
+  isEdit = false,
+  currentMember,
+}: Props) {
+  const [image, setImage] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
+  const { push, query } = useRouter();
+  const { memberId } = query;
+  const { enqueueSnackbar } = useSnackbar();
+
+  let NewMemberSchema;
+  if (isEdit) {
+    NewMemberSchema = Yup.object().shape({
+      firstName: Yup.string(),
+      lastName: Yup.string(),
+      email: Yup.string().email(),
+      password: Yup.string(),
+      address: Yup.string(),
+      phoneNumber: Yup.string(),
+      role: Yup.string().oneOf(["member"], 'Role must be "member"'),
+      avatarUrl: Yup.string(),
+    });
+  } else {
+    NewMemberSchema = Yup.object().shape({
+      firstName: Yup.string().required("First Name is required"),
+      lastName: Yup.string().required("Last Name is required"),
+      email: Yup.string().required("Email is required").email(),
+      password: Yup.string().required("Password is required"),
+      address: Yup.string(),
+      phoneNumber: Yup.string(),
+      role: Yup.string()
+        .required("Role is required")
+        .oneOf(["member"], 'Role must be "member"'),
+      avatarUrl: Yup.string(),
+    });
+  }
+
+  const defaultValues = useMemo(
+    () => ({
+      firstName: currentMember?.firstName || "",
+      lastName: currentMember?.lastName || "",
+      email: currentMember?.email || "",
+      phoneNumber: currentMember?.phoneNumber || "",
+      status: currentMember?.status,
+      role: "member",
+      address: currentMember?.address || "",
+      avatarUrl: currentMember?.avatarUrl?.secure_url || "",
+      password: currentMember?.password || "",
+    }),
+    [currentMember]
+  );
+
+  const methods = useForm<FormValuesProps>({
+    resolver: yupResolver(NewMemberSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  useEffect(() => {
+    if (isEdit && currentMember) {
+      reset(defaultValues);
+    }
+    if (!isEdit) {
+      reset(defaultValues);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isEdit, currentMember]);
+
+  const updateMemberMutation = useMutation(
+    (data: any) => updateMember(memberId as string, data),
+    {
+      onSuccess(data) {
+        enqueueSnackbar(data.message);
+        push(PATH_DASHBOARD.dashboard.admin.members.list);
+      },
+      onError(err: any) {
+        enqueueSnackbar(
+          err.response.data.message ??
+            err.message ??
+            err.data.message ??
+            "Something went wrong",
+          { variant: "error" }
+        );
+      },
+    }
+  );
+
+  const createMemberMutation = useMutation((data: any) => createMember(data), {
+    onSuccess(data) {
+      enqueueSnackbar(data.message);
+      push(PATH_DASHBOARD.dashboard.admin.members.list);
+      reset();
+    },
+    onError(err: any) {
+      enqueueSnackbar(
+        err.response.data.message ??
+          err.message ??
+          err.data.message ??
+          "Something went wrong",
+        { variant: "error" }
+      );
+    },
+  });
+  const onSubmit = async (data: FormValuesProps) => {
+    try {
+      console.log("onSubmit data", data);
+      const {
+        firstName,
+        lastName,
+        email,
+        password,
+        phoneNumber,
+        role,
+        address,
+      } = data;
+
+      const newData = { ...data, image };
+      const newEditData = { ...data, image };
+
+      console.log("SUBMITTED EDI TDATA", newEditData);
+      isEdit
+        ? updateMemberMutation.mutate(newEditData)
+        : createMemberMutation.mutate(newData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // const handleDrop = useCallback(
+  //   (acceptedFiles: File[]) => {
+  //     console.log({ acceptedFiles });
+  //     const file = acceptedFiles[0];
+  //     if (file) {
+  //       console.log({ file });
+  //       console.log({ acceptedFiles });
+  //       const avatar = values.avatarUrl;
+  //       console.log({ avatar });
+  //       setValue(
+  //         "avatarUrl",
+  //         Object.assign(file, {
+  //           preview: URL.createObjectURL(file),
+  //         })
+  //       );
+  //     }
+  //   },
+  //   [setValue]
+  // );
+
+  const handleDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const file = acceptedFiles[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+          const base64data = reader.result as string;
+
+          console.log("typeof base64", typeof base64data);
+
+          console.log({ base64data });
+          setImage(base64data);
+          console.log({ image });
+          setValue(
+            "avatarUrl",
+            Object.assign(file, {
+              preview: base64data,
+            })
+          );
+        };
+      }
+    },
+    [setValue]
+  );
+  // const handleDrop = useCallback(
+  //   async (acceptedFiles: File[]) => {
+  //     console.log("accepted files!!!!", acceptedFiles);
+
+  //     //cloudinary url with cloud name
+  //     const url = `https://api.cloudinary.com/v1_1/degovxk3x`;
+  //     const response = await axios.post(url);
+  //   },
+  //   [setValue]
+  // );
+
+  // const handleDrop = useCallback(
+  //   (acceptedFiles: File[]) => {
+  //     const file = acceptedFiles[0];
+  //     const reader = new FileReader();
+  //     reader.onload = () => {
+  //       console.log("reader result", reader.result);
+  //       const customFile: CustomFile = Object.assign(file, {
+  //         preview: URL.createObjectURL(reader.result as any),
+  //       });
+  //       setValue("avatarUrl", customFile);
+  //       console.log("customFile", customFile);
+  //     };
+
+  //     reader.readAsDataURL(file);
+  //   },
+  //   [setValue]
+  // );
+
+  // const setFileToBase = (file) =>{
+  //   const reader = new FileReader();
+  //   reader.readAsDataURL(file);
+  //   reader.onloadend = () =>{
+  //     setValue("avatarUrl")
+  //   }
+  // }
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4}>
+          <Card sx={{ py: 7, px: 3, position: "relative" }}>
+            {isEdit && (
+              <Button
+                size="small"
+                variant="contained"
+                color={values.status !== "active" ? "error" : "success"}
+                sx={{
+                  position: "absolute",
+                  top: 30,
+                  right: 10,
+                }}
+              >
+                <Typography fontSize="12px">Inactive</Typography>
+              </Button>
+            )}
+
+            <Box sx={{ mb: 5 }}>
+              <RHFUploadAvatar
+                name="avatarUrl"
+                onDrop={handleDrop}
+                helperText={
+                  <Typography
+                    variant="caption"
+                    sx={{
+                      mt: 2,
+                      mx: "auto",
+                      display: "block",
+                      textAlign: "center",
+                      color: "text.secondary",
+                    }}
+                  >
+                    Profile Picture
+                  </Typography>
+                }
+              />
+            </Box>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Card sx={{ p: 3 }}>
+            <Box
+              sx={{
+                display: "grid",
+                columnGap: 2,
+                rowGap: 3,
+                gridTemplateColumns: {
+                  xs: "repeat(1,1fr)",
+                  sm: "repeat(2,1fr)",
+                },
+              }}
+            >
+              <RHFTextField name="firstName" label="First Name" />
+              <RHFTextField name="lastName" label="Last Name" />
+              <RHFTextField name="email" label="Email" />
+              <RHFTextField
+                name="password"
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => setShowPassword(!showPassword)}
+                        edge="end"
+                      >
+                        <Iconify
+                          icon={
+                            showPassword ? "eva:eye-fill" : "eva:eye-off-fill"
+                          }
+                        />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <RHFTextField name="phoneNumber" label="Phone Number" />
+              <RHFTextField name="address" label="Address" />
+              <RHFTextField name="role" label="Role" disabled />
+            </Box>
+
+            <Stack alignItems="flex-end" sx={{ mt: 3 }}>
+              <LoadingButton
+                type="submit"
+                variant="contained"
+                loading={isSubmitting}
+              >
+                {!isEdit ? "Create Member" : "Save Changes"}
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
+    </FormProvider>
+  );
+}
