f3fe100562891fe558f5276b4f982d4a7581cdc0
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-03-11 20:56:02 +0545

    Added profile picture upload feature

---
+++ src/components/hook-form/RHFUpload.tsx
@@ -0,0 +1,62 @@
+import { ReactNode } from "react";
+import { DropzoneOptions } from "react-dropzone";
+import { SxProps } from "@mui/material";
+import { Theme } from "@mui/material/styles";
+import { Controller, useFormContext } from "react-hook-form";
+import FormHelperText from "@mui/material/FormHelperText";
+import UploadAvatar from "../upload/UploadAvatar";
+export interface CustomFile extends File {
+  path?: string;
+  preview?: string;
+  lastModifiedDate?: Date;
+}
+
+export interface UploadProps extends DropzoneOptions {
+  error?: boolean;
+  file: CustomFile | string | null;
+  helperText?: ReactNode;
+  sx?: SxProps<Theme>;
+}
+
+export interface UploadMultiFileProps extends DropzoneOptions {
+  files: (File | string)[];
+  error?: boolean;
+  showPreview?: boolean;
+  sx?: SxProps<Theme>;
+  helperText?: ReactNode;
+  onUpload?: VoidFunction;
+  onRemove?: (file: File | string) => void;
+  onRemoveAll?: VoidFunction;
+}
+interface Props extends Omit<UploadProps, "file"> {
+  name: string;
+}
+export function RHFUploadAvatar({ name, ...other }: Props) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => {
+        const checkError = !!error && !field.value;
+
+        return (
+          <div>
+            <UploadAvatar
+              accept="image/*"
+              error={checkError}
+              {...other}
+              file={field.value}
+            />
+            {checkError && (
+              <FormHelperText error sx={{ px: 2, textAlign: "center" }}>
+                {error.message}
+              </FormHelperText>
+            )}
+          </div>
+        );
+      }}
+    />
+  );
+}
