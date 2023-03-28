352f8d0d1a945eaf01e45616e0556f5c4c018ebe
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-03-28 17:38:13 +0545

    Created tables and forms needed for staffs

---
+++ src/sections/auth/dashboard/staff-list/StaffTableRow.tsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+
+// @mui
+import { useTheme } from "@mui/material/styles";
+import {
+  TableRow,
+  Checkbox,
+  TableCell,
+  Typography,
+  MenuItem,
+  Link,
+  Button,
+  Avatar,
+} from "@mui/material";
+// utils
+
+import { useRouter } from "next/router";
+import Iconify from "../../../../components/Iconify";
+import { PATH_DASHBOARD } from "../../../../routes/path";
+import { TableMoreMenu } from "../../../../components/table";
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  row: any;
+  selected: boolean;
+  onEditRow: VoidFunction;
+  onSelectRow: VoidFunction;
+  onDeleteRow: VoidFunction;
+};
+
+export default function StaffTableRow({
+  row,
+  selected,
+  onSelectRow,
+  onDeleteRow,
+  onEditRow,
+}: Props) {
+  const theme = useTheme();
+  const { pathname, push } = useRouter();
+  const { firstName, lastName, email, avatarUrl } = row;
+  console.log("row!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", row);
+  const [openMenu, setOpenMenuActions] = useState<HTMLElement | null>(null);
+
+  const handleOpenMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setOpenMenuActions(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setOpenMenuActions(null);
+  };
+
+  return (
+    <TableRow hover selected={selected}>
+      <TableCell padding="checkbox">
+        <Checkbox checked={selected} onClick={onSelectRow} />
+      </TableCell>
+
+      <TableCell sx={{ display: "flex", alignItems: "center" }}>
+        <Avatar alt={firstName} src={avatarUrl?.secure_url} sx={{ mr: 2 }} />
+        <Link
+          component="button"
+          variant="caption"
+          onClick={() => {
+            // push(PATH_DASHBOARD.menu.customers.view(paramCase(id)));
+          }}
+        >
+          <Typography variant="subtitle2" noWrap>
+            {firstName}
+          </Typography>
+        </Link>
+      </TableCell>
+
+      <TableCell>{lastName}</TableCell>
+
+      {/* <TableCell align="center">
+        <Label
+          variant={theme.palette.mode === 'light' ? 'ghost' : 'filled'}
+          color={
+            (unit === 'out_of_stock' && 'error') ||
+            (unit === 'low_stock' && 'warning') ||
+            'success'
+          }
+          sx={{ textTransform: 'capitalize' }}
+        >
+          {unit ? sentenceCase(unit) : ''}
+        </Label>
+      </TableCell> */}
+
+      <TableCell align="left">{email}</TableCell>
+
+      <TableCell align="right">
+        <TableMoreMenu
+          open={openMenu}
+          onOpen={handleOpenMenu}
+          onClose={handleCloseMenu}
+          actions={
+            <>
+              <MenuItem
+                onClick={() => {
+                  onDeleteRow();
+                  handleCloseMenu();
+                }}
+                sx={{ color: "error.main" }}
+              >
+                <Iconify icon={"eva:trash-2-outline"} />
+                Delete
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  onEditRow();
+                  handleCloseMenu();
+                }}
+              >
+                <Iconify icon={"eva:edit-fill"} />
+                Edit
+              </MenuItem>
+            </>
+          }
+        />
+      </TableCell>
+      {/* </Link> */}
+    </TableRow>
+  );
+}
---
+++ src/sections/auth/dashboard/staff-list/StaffTableToolbar.tsx
@@ -0,0 +1,51 @@
+// @mui
+import {
+  Tooltip,
+  IconButton,
+  Stack,
+  InputAdornment,
+  TextField,
+} from "@mui/material";
+import Iconify from "../../../../components/Iconify";
+
+// components
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  filterName: string;
+  onFilterName: (value: string) => void;
+};
+
+export default function StaffTableToolbar({ filterName, onFilterName }: Props) {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      sx={{ py: 2.5, px: 3 }}
+    >
+      <TextField
+        value={filterName}
+        onChange={(event) => onFilterName(event.target.value)}
+        placeholder={"Search Trainer" + "..."}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Iconify
+                icon={"eva:search-fill"}
+                sx={{ color: "text.disabled", width: 20, height: 20 }}
+              />
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <Tooltip title="Filter list">
+        <IconButton>
+          <Iconify icon={"ic:round-filter-list"} />
+        </IconButton>
+      </Tooltip>
+    </Stack>
+  );
+}
