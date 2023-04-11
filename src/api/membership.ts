3b73065d8b1f0ba536fd3804dee5c94c6263be55
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-04-11 10:08:15 +0545

    Updated membership API

--- src/api/membership.ts
+++ src/api/membership.ts
@@ -3,3 +3,21 @@ export const getOneMembership = async (id: string) => {
   const { data } = await axios.get(`/api/membership/${id}`);
   return data.data;
 };
+export const getMembership = async ({
+  page,
+  limit,
+  searchQuery,
+  sortBy,
+  order,
+}: any) => {
+  const { data } = await axios.get(`/api/membership`, {
+    params: {
+      pageNumber: page,
+      pageSize: limit,
+      searchQuery,
+      sortBy,
+      order,
+    },
+  });
+  return data.data;
+};
